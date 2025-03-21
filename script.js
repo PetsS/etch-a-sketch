7234347c2991174ead462f639f832e7ba1e19888
Peter Szots <103681468+PetsS@users.noreply.github.com>
2025-03-21 15:51:48 +0100

    create 16x16 grid

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+// get container from html
 const gridContainer = document.querySelector("#grid-container");
+
+// greate the grid (16x16 grid, 256 blocks) and append to container
+for (let i = 0; i < 256; i++) {
+  // create a div
+  const div = document.createElement("div");
+  // styling the div
+  div.setAttribute(
+    "style",
+    "border: 1px solid black; width: 58px; height: 58px"
+  );
+  gridContainer.appendChild(div);
+}
 
